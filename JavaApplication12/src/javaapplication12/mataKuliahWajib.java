50e29c98c50e77eabc91d5a10fc2396bb7fb6b6b
maulananur30 <maulananur30@gmail.com>
2022-01-04 21:08:55 +0700

    Clas mk pilihan dan wajib selesai

--- JavaApplication12/src/javaapplication12/mataKuliahWajib.java
+++ JavaApplication12/src/javaapplication12/mataKuliahWajib.java
@@ -10,5 +10,19 @@ package javaapplication12;
  * @author amoro
  */
 public class mataKuliahWajib {
+    String namaMataKuliah;
+    Dosen Dosen;
     
+    public mataKuliahWajib(String mataKuliah, Dosen Dosen){
+        this.namaMataKuliah = mataKuliah;
+        this.Dosen = Dosen;
+    }
+    
+    public void getMatakuliah(String namaMataKuliah){
+        this.namaMataKuliah = namaMataKuliah;
+    }
+    
+    public void getDosen (Dosen Dosen){
+        this.Dosen = Dosen;
+    }
 }
