4da5510be5a9e910bc35685991873cf708c0910a
Fendi Irfan Amorokhman <amorokhman@gmail.com>
2022-01-04 15:55:00 +0800

    init class file

--- JavaApplication12/src/javaapplication12/mataKuliah.java
+++ JavaApplication12/src/javaapplication12/mataKuliah.java
@@ -9,13 +9,6 @@ package javaapplication12;
  *
  * @author amoro
  */
-public class JavaApplication12 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class mataKuliah {
     
 }
